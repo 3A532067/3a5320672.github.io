27cf7fa1f2e956b1387064d91ce348f4a9c5a90d
3A532067 <s3a532067@student.ncut.edu.tw>
2019-12-24 07:59:42 +0800

    photo album v1.0

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
- $("#owl-example").owlCarousel({
-    // Most important owl features
-    items : 4,
-    pagination : true,
-    paginationSpeed : 1000,
-    navigation : true,
-    navigationText : ["","<i class='fa fa-angle-right'></i>"],
-    slideSpeed : 800,
- });
-
-	$("#navigation").sticky({
-		topSpacing : 75,
-	});
-
-	$('#nav').onePageNav({
-		currentClass: 'current',
-		changeHash: false,
-		scrollSpeed: 15000,
-		scrollThreshold: 0.5,
-		filter: '',
-		easing: 'easeInOutExpo'
-	});
-
-     $('#top-nav').onePageNav({
-         currentClass: 'active',
-         changeHash: true,
-         scrollSpeed: 1200
+    $("#owl-example").owlCarousel({
+        // Most important owl features
+        items: 4,
+        pagination: true,
+        paginationSpeed: 1000,
+        navigation: true,
+        navigationText: ["", "<i class='fa fa-angle-right'></i>"],
+        slideSpeed: 800,
     });
-//Initiat WOW JS
+
+    $("#navigation").sticky({
+        topSpacing: 75,
+    });
+
+    $('#nav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 15000,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'easeInOutExpo'
+    });
+
+    $('#top-nav').onePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+    //Initiat WOW JS
     new WOW().init();
 
 });
+
+function upDate(previewPic) {
+    var imgURL = previewPic.src;
+
+    document.getElementById("preview").style.backgroundColor = "#CCEECC"
+    document.getElementById("preview").style.backgroundImage = "url(" + imgURL + ")";
+    document.getElementById("preview").style.backgroundSize = "cover";
+}
+
+function unDo() {
+    document.getElementById("preview").style.backgroundColor = "#8e68ff"
+    document.getElementById("preview").innerHTML = "Preview";
+    document.getElementById("preview").style.backgroundImage = "none";
+}
